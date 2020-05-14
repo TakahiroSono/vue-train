029c6b52be3bf803914f57f88717cf0afd0c8bf8
TakahiroSono <taka.sono889@gmail.com>
2020-05-14 16:39:42 +0900

    fix responsive page

--- src/page/Responsive.vue
+++ src/page/Responsive.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="responsive">
-    <ul>
-      <li>要素1</li>
-      <li>要素2</li>
-      <li>要素3</li>
-    </ul>
+    <div class="contents">
+      <div class="side">
+        <div class="box__1">要素1</div>
+        <div class="box__3">要素3</div>
+      </div>
+      <div class="box__2">要素2</div>
+    </div>
   </div>
 </template>
 
@@ -19,8 +21,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-ul {
-  list-style: none;
+$up-lg: 901px; $dw-lg: 900px;
+$up-sm: 601px; $dw-sm: 600px;
+
+%item {
+  height: 100px;
+  line-height: 100px;
+  color: #fff;
+  display: inline-block;
+}
+
+.contents {
+  position: relative;
+}
+.side {
   display: flex;
+  justify-content: space-between;
+  @media screen and (max-width: $dw-sm) {
+    flex-wrap: wrap;
+  }
+}
+.box {
+  &__1, &__3 {
+    @extend %item;
+    width: 30%;
+    @media screen and (max-width: $dw-lg) {
+      width: 50%;
+    }
+    @media screen and (max-width: $dw-sm) {
+      width: 100%;
+    }
+  }
+  &__1{
+    background-color: #940000;
+  }
+  &__2{
+    @extend %item;
+    background-color: #0b770b;
+    width: 100%;
+    @media screen and (min-width: $up-lg) {
+      width: 40%;
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+  &__3{
+    background-color: #0000a7;
+  }
 }
 </style>
